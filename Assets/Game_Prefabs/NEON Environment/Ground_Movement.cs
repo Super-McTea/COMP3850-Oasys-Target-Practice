d4683403ac23a858c580ed2685eee4c71946d710
45848270 <shubhamchandra8048@gmail.com>
2022-09-11 17:50:21 +1000

    neon environment

---
+++ Assets/Game_Prefabs/NEON Environment/Ground_Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground_Movement : MonoBehaviour
+{
+    public float movementSpeed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Time.deltaTime * -1 * transform.forward * movementSpeed;
+    }
+}
