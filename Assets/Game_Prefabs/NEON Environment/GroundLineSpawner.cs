b89f69149071b062c71edb02f9f568ad06593a0e
45848270 <shubhamchandra8048@gmail.com>
2022-09-11 19:08:30 +1000

    basic environmental stuff

--- Assets/Game_Prefabs/NEON Environment/GroundLineSpawner.cs
+++ Assets/Game_Prefabs/NEON Environment/GroundLineSpawner.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class GroundLineSpawner : MonoBehaviour
 {
+    public GameObject cubes;
+    public float timer=1.5f;
+
+    private float time1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        time1 = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(time1<=0)
+        {
+            Instantiate(cubes, transform.position,Quaternion.identity);
+            time1 = timer;
+        }
+        time1 -= Time.deltaTime;
     }
 }
--- Assets/Game_Prefabs/NEON Environment/Ground_Movement.cs
+++ Assets/Game_Prefabs/NEON Environment/Ground_Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Ground_Movement : MonoBehaviour
 {
     public float movementSpeed = 2;
+    public float destroy_time=50f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +16,11 @@ public class Ground_Movement : MonoBehaviour
     void Update()
     {
         transform.position += Time.deltaTime * -1 * transform.forward * movementSpeed;
+        destroy_time -= Time.deltaTime;
+
+        if(destroy_time<=0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
