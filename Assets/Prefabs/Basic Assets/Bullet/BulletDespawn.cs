4121e69adba499eb17f79b8b36f4e70c3b207686
45848270 <shubhamchandra8048@gmail.com>
2022-10-06 18:19:23 +1100

    main scene with gun

---
+++ Assets/Prefabs/Basic Assets/Bullet/BulletDespawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDespawn : MonoBehaviour
+{
+    [Range(1, 10)]
+    public float killTime = 3;
+
+    float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        
+        if (time >= killTime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
